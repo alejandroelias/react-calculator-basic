3b77294b0a7883a2d713ed3e8ae1b3e18d1cb3ae
alejandroelias <alfredo.elias@outlook.com>
2021-12-15 09:46:28 -0600

    Iniciando con solucion segun requerimientos

--- src/component/CalculatorApp.js
+++ src/component/CalculatorApp.js
@@ -11,6 +11,20 @@ export const CalculatorApp = () => {
     const initialState = "";
     const [calc, setCalc] = useState(initialState);
     const [result, setResult] = useState(initialState);
+
+
+    const updateCalc = (value) => {
+        if (
+            (operators.includes(value) && calc.slice === '') ||
+            (operators.includes(value) && operators.includes(calc.slice(-1)))
+        ) {
+            return;
+        }
+        setCalc(calc + value);
+
+        if (!operators.includes(value)) {
+            setResult(eval(calc + value).toString());
+        }
     }
 
     const calculate = () => {
